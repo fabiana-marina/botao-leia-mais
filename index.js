c27bac7a041921649372aa2945ad0c6dbfa91456
Fabiana Almeida <fabianamarinaprog@gmail.com>
2021-08-06 02:14:22 -0300

    version 01

---
+++ index.js
@@ -0,0 +1,11 @@
+function lerMais(){
+  document.getElementById("areaDeTexto").style.height="1250px";
+  document.getElementById("button1").style.display="none";
+  document.getElementById("button2").style.display="block";
+    }
+
+function lerMenos(){
+  document.getElementById("areaDeTexto").style.height="500px";
+  document.getElementById("button2").style.display="none";
+  document.getElementById("button1").style.display="block";
+    }
